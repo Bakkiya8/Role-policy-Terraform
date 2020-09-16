ea7ade141ba0c3a208b91f0ca4af27d3cdd8b2bb
Bakkiya8 <52496334+Bakkiya8@users.noreply.github.com>
2020-09-16 17:05:12 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role_policy" "iam_lamda_policy" {
   name = "iam_lamda_policy"
   role = aws_iam_role.lamda_role.id
-  policy = "${file("lamda.json")}"
+  policy = "${file("policy.json")}"
 }
 
 resource "aws_iam_role" "lamda_role" {
